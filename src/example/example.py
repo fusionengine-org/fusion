cddf9c859119c79575c072367621e28f5125b65c
Dima Marhitych <uzhdimka@gmail.com>
2023-06-09 12:04:05 +0200

    Just some testing with the engine

--- src/example/example.py
+++ src/example/example.py
@@ -8,11 +8,13 @@ main = engine.Main()
 window = main.window.newWindow("Example: 1", 800, 600)
 
 while main.window.running:
+    '''
     main.draw.setBackgroundColor(window, main.color.WHITE)
     main.draw.drawRect(window, 100, 100, 400, 400, main.color.BLUE)
 
     if main.event.keyDown(main.keys.KEY_a, window):
         print("Key A pressed")
+    '''
 
     main.window.refresh(window)
 
