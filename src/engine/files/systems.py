2830b67cb4a3fac7b1696ce6208fedaea66e4580
dimkauzh <106883655+dimkauzh@users.noreply.github.com>
2023-06-28 12:02:02 +0200

    Update rendering system

--- src/engine/files/systems.py
+++ src/engine/files/systems.py
@@ -6,14 +6,14 @@ class RendererOptions:
     def getSurfaceFromWindow(self, window):
         return window.surface
     def setRendererFlag(self, type):
-        if type == "accelerated" or type == "ACCELERATED":
-            self.rendererflag = SDL_RENDERER_ACCELERATED
-        elif type == "PRESENTVSYNC" or type == "presentvsync":
+        if type == "PRESENTVSYNC" or type == "presentvsync":
             self.rendererflag = SDL_RENDERER_PRESENTVSYNC
         elif type == "software" or type == "SOFTWARE":
             self.rendererflag = SDL_RENDERER_SOFTWARE
         elif type == "TARGETTEXTURE" or type == "targettexture":
             self.rendererflag = SDL_RENDERER_TARGETTEXTURE
+        else:
+            self.rendererflag = SDL_RENDERER_ACCELERATED
 
 class System:
     def __init__(self):
