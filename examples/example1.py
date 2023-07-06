feb641cccbd682e5917c92f8ab2d92118ff58de1
dimkauzh <106883655+dimkauzh@users.noreply.github.com>
2023-07-06 12:08:17 +0200

    Centred image example

--- examples/example1.py
+++ examples/example1.py
@@ -3,7 +3,7 @@ import fusionengine as fusion
 main = fusion.Main()
 
 window = main.window.new_window("Example: 1", 800, 600)
-image = main.image.open_image(window, main.DEBUGIMAGE, 100, 100, 400, 400)
+image = main.image.open_image(window, main.DEBUGIMAGE, 300, 200, 200, 200)
 
 @main.window.loop
 def loop():
