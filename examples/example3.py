6e42988c64171dc35784cabf5b48093978a84ac9
Dima Marhitych <uzhdimka@gmail.com>
2023-07-28 20:18:00 +0200

    Update key system and one of the examples

--- examples/example3.py
+++ examples/example3.py
@@ -4,12 +4,29 @@ main = fusion.Main()
 
 window = main.window.new_window("Example: 3", 800, 600)
 
-player = main.body.Entity("rigid", window, 100, 100, 50, 50)
+player = main.body.Entity("rigid", window, 70, 40, 50, 50)
+
+
+speed = 10
+
 
 @main.window.loop
 def loop():
-
     main.draw.set_background_color(window, main.color.WHITE)
-    main.draw.draw_rect(window, 100, 100, 400, 400, main.color.BLUE)
+
+    player.new_rect(window, main.color.AQUA)
+
+    if main.event.key_down(window, main.keys.KEY_UP):
+        player.y = int(player.y - speed)
+
+    elif main.event.key_down(window, main.keys.KEY_DOWN):
+        player.y = int(player.y + speed)
+
+    elif main.event.key_down(window, main.keys.KEY_RIGHT):
+        player.x = int(player.x + speed)
+
+    elif main.event.key_down(window, main.keys.KEY_LEFT):
+        player.x = int(player.x - speed)
+
 
 main.quit(window)
