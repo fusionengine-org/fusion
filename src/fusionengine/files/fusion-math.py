e524419a2de96951791a79a94d35318b19b30422
nmsderp <130254323+nmsderp@users.noreply.github.com>
2023-08-16 18:11:41 -0400

    ea sports

--- src/fusionengine/files/fusion-math.py
+++ src/fusionengine/files/fusion-math.py
@@ -1,5 +1,6 @@
 # this is used for extra debugging features
 # tbh this might get removed
 class Math:
-    # most likely useless but i put it here anyway
-    self.VALUE-OF-PI = 3.141592653589793238462643383279502884197.
+    def __init__(self):
+        # most likely useless but i put it here anyway
+        self.VALUE-OF-PI = 3.141592653589793238462643383279502884197.
