38575d3ad4bc4fb9a4466d04f14716087d32c378
nmsderp <130254323+nmsderp@users.noreply.github.com>
2023-08-16 18:05:22 -0400

    baller

--- src/fusionengine/files/fusion-math.py
+++ src/fusionengine/files/fusion-math.py
@@ -1,5 +1,5 @@
 # this is used for extra debugging features
 # tbh this might get removed
-class Fusion-math:
+class Math:
     # most likely useless but i put it here anyway
     self.VALUE-OF-PI = 3.141592653589793238462643383279502884197.
