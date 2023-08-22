c61e113d960e81db8a010acf66c933cade2fbd21
Dima Marhitych <uzhdimka@gmail.com>
2023-08-22 22:33:58 +0200

    Discord module is finally back

---
+++ discord/send_webhook.py
@@ -0,0 +1,24 @@
+from discord_webhook import DiscordWebhook
+import fusionengine as fusion
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def send_discord_webhook(message):
+    webhook = os.environ.get("DISCORD_WEBHOOK")
+
+    if webhook:
+        webhook = DiscordWebhook(url=webhook, content=message)
+        webhook.execute()
+    else:
+        print("Discord webhook URL not found in environment variables.")
+
+
+version = fusion.__version__
+author = fusion.__author__
+
+message = f"Fusion Engine {version} of package has been released to PyPI by {author}! Check it out at https://pypi.org/project/fusion-engine/{version}"
+
+send_discord_webhook(message)
