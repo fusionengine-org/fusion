db58a354a8c9dfb008177da057bea0213854a7c8
Dima Marhitych <uzhdimka@gmail.com>
2023-11-08 10:07:07 +0100

    Cleaning the code

--- src/fusionengine/files/window.py
+++ src/fusionengine/files/window.py
@@ -2,62 +2,25 @@ import fusionengine.files.debug as debugfe
 from fusionengine.files.imports import *
 
 
-class _CustomRenderer:
-    def __init__(
-        self, window: pg.Surface, title: str, width: int, height: int, manager
-    ) -> None:
-        """A class that creates a new custom renderer. (Not for the user)
-
-        Args:
-            window (sdl2.SDL_CreateWindow): An created sdl2 window
-        """
-        self.window = window
-
-        self.title = title
-        self.width = width
-        self.height = height
-        self.size = (self.width, self.height)
-
-        self.manager = manager
-
-
 class Window:
-    def __init__(self) -> None:
-        """A class that contains all the window functions."""
+    def __init__(self, title: str, width: int, height: int) -> None:
         self._running = False
         self._fps = 60
         self._quittable = True
         self.clock = pg.time.Clock()
 
-    def new_window(self, title: str, width: int, height: int) -> _CustomRenderer:
-        """Creates a new window.
-
-        Args:
-            title (str): Your window title
-            width (int): Your window width
-            height (int): Your window height
-
-        Returns:
-            window: Custom window class with all you need features
-        """
         try:
-            window_window = pg.display.set_mode((width, height))
+            self.window = pg.display.set_mode((width, height))
             pg.display.set_caption(title)
 
             self.manager = gui.UIManager((800, 600))
 
-            programIcon = pg.image.load(debugfe.DebugFiles().DEBUGIMAGE)
-            pg.display.set_icon(programIcon)
+            program_icon = pg.image.load(debugfe.DebugFiles().DEBUGIMAGE)
+            pg.display.set_icon(program_icon)
 
             self._running = True
-            self.window = _CustomRenderer(
-                window_window, title, width, height, self.manager
-            )
-
         except Exception:
             print("Error: Can't create a window.")
-
-        return self.window
 
     def change_icon(self, image_path):
         """Changes icon
@@ -76,10 +39,10 @@ class Window:
         Args:
             your_loop (callable): Your main loop function
         """
-        while self.running(self.window):
+        while self.running():
             your_loop()
 
-    def running(self, window: _CustomRenderer) -> bool:
+    def running(self) -> bool:
         """Returns if the window is running. Used for the main loop.
 
         Args:
@@ -88,7 +51,7 @@ class Window:
         Returns:
             bool: returns true if the window is running else false
         """
-        self._refresh(window)
+        self._refresh()
         return self._running
 
     def set_fps(self, fps: int) -> None:
@@ -113,7 +76,7 @@ class Window:
         """Toggles whether the window is quittable."""
         self._quittable = not self._quittable
 
-    def _refresh(self, window: _CustomRenderer) -> None:
+    def _refresh(self) -> None:
         """Does all things for refreshing window. (Not for the user)
 
         Args:
@@ -129,8 +92,8 @@ class Window:
             self.manager.process_events(event)
 
         self.manager.update(self.DELTATIME)
-        self.manager.draw_ui(window.window)
+        self.manager.draw_ui(self.window)
 
         pg.display.flip()
 
-        window.window.fill((0, 0, 0))
+        self.window.fill((0, 0, 0))
