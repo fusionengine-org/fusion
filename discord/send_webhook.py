2eff52fd68d9a0ecb37d84e22657e0042fe3a94b
Dima Marhitych <uzhdimka@gmail.com>
2023-12-31 22:36:38 +0100

    Update webhook

--- discord/send_webhook.py
+++ discord/send_webhook.py
@@ -1,22 +1,22 @@
 from discord_webhook import DiscordWebhook
 import fusionengine as fusion
+from dotenv import load_dotenv
 import os
 
-
-def send_discord_webhook(message):
-    if webhook := os.environ.get("FUSION_WEBHOOK"):
-        DiscordWebhook(url=webhook, content=message).execute()
-    else:
-        print("Discord webhook URL not found in environment variables.")
-
+load_dotenv()
 
 version = fusion.__version__
 author = fusion.__author__
 
 message = f"Fusion Engine {version} of package has been released to PyPI by {author}! Check it out at https://pypi.org/project/fusion-engine/{version}"
 
-print("Sending webhook...")
+if webhook := os.getenv("FUSION_WEBHOOK"):
+    print("Sending webhook...")
 
-send_discord_webhook(message)
+    DiscordWebhook(url=webhook, content=message).execute()
 
-print("Webhook sent!")
+    print("Webhook sent!")
+    
+else:
+    print("Discord webhook URL not found in environment variables.")
+
